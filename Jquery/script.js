15e9495fc75fe6e5f728207988e2f9201a273c12
Saluki-scribe <tara@nelsonwriting.com>
2017-12-07 18:55:19 -0500

    Added practice code.

--- Jquery/script.js
+++ Jquery/script.js
@@ -4,6 +4,23 @@ $(function() {
         $("#box").fadeOut(1000);
     });
 
-    $("h2, h3, h4").css("border", "solid 1px red");
+    //$("h2, h3, h4").css("border", "solid 1px red");
+
+    //$("div#container").css("border", "dotted 2px green");
+
+    //$("p.lead").css("border", "dashed 3px blue");
+
+    //$("li:first").css("border", "double 3px orange");
+
+    //$("p:even").css("border", "solid 2px blue");
+    //$("p:odd").css("border", "solid 2px red");
+
+    //$("div em").css("border", "solid 1px red");
+
+    //$("div > p").css("border", "dashed 2px green");
+
+    //$(":header").css("color", "red");
+
+    //$("div:contains('Tara')").css("color", "red");
 
 });
