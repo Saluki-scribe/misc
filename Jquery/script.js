13107b17e0f9d3fc66393f0a20a0b2caca5bcfa1
Saluki-scribe <tara@nelsonwriting.com>
2017-12-13 23:24:31 -0500

    Added classes via jQuery

--- Jquery/script.js
+++ Jquery/script.js
@@ -127,7 +127,19 @@ $("#down").click(function() {
 
 });
 
+/* -----------------
+jQUERY CSS
+-------------------*/
 
+$("#circle2").css({ 
+    "background": "#8a8d22", 
+    "display": "inline-block", 
+    "color": "white", 
+    "text-align": "center", 
+    "line-height": "140px", 
+    "height": "140px", 
+    "width": "140px"
+});
 
 
 
