de670fa79b1e61fe07bd14c40329e64aa54c2484
Saluki-scribe <tara@nelsonwriting.com>
2018-01-12 17:23:59 -0500

    Added functions and displayed results to page.

--- PHP/clickbait.php
+++ PHP/clickbait.php
@@ -2,7 +2,7 @@
     define("TITLE", "Recipes for Monsters");
 
     if( isset( $_POST["fix_submit"] )) {
-        $clickBait = strtolower($_POST["clickbait_headline"]);
+        $recipe = strtolower($_POST["normal_recipe"]);
 
         $a = array(
             "milk",
@@ -34,6 +34,7 @@
             "ashes"
         );
 
+        $monsterRecipe = str_replace( $a, $b, $recipe);
     }
 ?>
 
@@ -54,16 +55,26 @@
     <body>
         <div class="container">
             <h1><?php echo(TITLE); ?></h1>
-            <p class="lead">Hate click bait? Turn those annoying headlines into realistic and honest ones using this simple program.</p>
+            <p class="lead">Are you expecting undead, monstrous, or otherwise supernaturally terrifying guests? Just add a run of the mill recipe, and we'll convert it into a meal any monster would love!</p>
 
             <div class="row">
                 <form class="col-sm-8 col-sm-offset-2" action="" method="post">
-                    <textarea placeholder="Paste click bait headline here" class="form-control input-lg" name="clickbait_headline"></textarea><br>
-                    <button type="submit" class="btn btn-primary btn-lg pull-right" name="fix_submit">Make Honest</button>
+                    <textarea placeholder="Paste recipe ingredients here" class="form-control input-lg" name="normal_recipe"></textarea><br>
+                    <button type="submit" class="btn btn-primary btn-lg pull-right" name="fix_submit">Make Monstrous!</button>
                 </form>
             </div>
 
-        </div>
+            <?php 
+                if (isset($_POST["fix_submit"])) {
+                    echo ("<strong class='text-danger'>Normal Recipe</strong>
+                    <h4>".$recipe. "</h4><hr>");
+
+                    echo ("<strong class='text-success'>Monster Recipe</strong>
+                    <h4>".$monsterRecipe. "</h4>");
+                }
+            ?>
+
+        </div> <!--End Container-->
 
     </body>
 
