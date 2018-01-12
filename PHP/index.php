08f8d16d18624b2f686c4836350538fc09ef21ab
Saluki-scribe <tara@nelsonwriting.com>
2018-01-12 13:20:57 -0500

    Completed lesson 1

--- PHP/index.php
+++ PHP/index.php
@@ -5,5 +5,32 @@
         <title></title>
     </head>
     <body>
+
+        <?php 
+            echo ("Hello world!");
+        ?>
+
+        <?php 
+            //Single line comment
+            # single line comment
+            /*
+            mul
+            ti
+            line
+            comment
+            */
+        ?>
+
+        <?php 
+        
+        prInt("I love you!<br>");
+        echo("A lot!");
+        
+        $name = "Tara";
+        echo "Hello " . $name . ".<br>";
+
+        ?>
+
+
     </body>
 </html>
