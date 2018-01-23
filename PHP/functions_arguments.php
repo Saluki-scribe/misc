c1ed6c33763f0707a7dbec27c8f1aba1c8a2aee9
Saluki-scribe <tara@nelsonwriting.com>
2018-01-23 09:04:59 -0500

    Created functions_arguments.php

---
+++ PHP/functions_arguments.php
@@ -0,0 +1,34 @@
+<?php 
+    define( "TITLE", "Functions &amp; Arguments");
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title><?php echo TITLE; ?></title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
+
+    <body>
+        <div class="container">
+            
+            <h1><?php echo TITLE; ?></h1>
+            
+            <?php 
+
+                function myFirstFunction () {
+                    
+                }
+
+            ?>
+        </div>
+
+    </body>
+</html>
