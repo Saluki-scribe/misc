4c57f50963325cccbba74ad6400fdc627a49f14e
Saluki-scribe <tara@nelsonwriting.com>
2018-01-23 09:20:44 -0500

    Created get_post.php file

---
+++ PHP/get_post.php
@@ -0,0 +1,30 @@
+<?php 
+    define( "TITLE", "Get &amp; Post");
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title><?php echo TITLE; ?></title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
+
+    <body>
+        <div class="container">
+            
+            <h1><?php echo TITLE; ?></h1>
+            
+            <?php 
+            
+            ?>
+        </div>
+    </body>
+
+</html>
